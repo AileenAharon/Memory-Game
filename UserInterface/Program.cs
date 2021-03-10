00eddf59b5b8261fc4abea54d490088011f04683
Aileen Aharon <74129678+AileenAharon@users.noreply.github.com>
2021-03-10 13:58:34 +0200

    Add files via upload

---
+++ UserInterface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+// $G$ SFN-009 (+10) Bonus: pictures in buttons
+
+namespace UserInterface
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            MemoryGameForm memoryGame = new MemoryGameForm();
+            memoryGame.ShowDialog();
+        }
+    }
+}
